30090de21e812c13f786b706e8f485c729f65631
ed bee <theedbee@hotmail.com>
2018-04-09 17:55:16 +0100

    UI and set up buttons

---
+++ MainProgram.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox_Genre_List = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btn_About = new System.Windows.Forms.Button();
+            this.PlayList1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox_Genre_List
+            // 
+            this.listBox_Genre_List.FormattingEnabled = true;
+            this.listBox_Genre_List.HorizontalScrollbar = true;
+            this.listBox_Genre_List.ItemHeight = 20;
+            this.listBox_Genre_List.Location = new System.Drawing.Point(215, 266);
+            this.listBox_Genre_List.Name = "listBox_Genre_List";
+            this.listBox_Genre_List.Size = new System.Drawing.Size(285, 104);
+            this.listBox_Genre_List.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(215, 245);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(285, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.textBox2.Location = new System.Drawing.Point(215, 385);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(285, 26);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // btn_About
+            // 
+            this.btn_About.Location = new System.Drawing.Point(12, 805);
+            this.btn_About.Name = "btn_About";
+            this.btn_About.Size = new System.Drawing.Size(101, 48);
+            this.btn_About.TabIndex = 4;
+            this.btn_About.Text = "About";
+            this.btn_About.UseVisualStyleBackColor = true;
+            this.btn_About.Click += new System.EventHandler(this.btn_About_Click);
+            // 
+            // PlayList1
+            // 
+            this.PlayList1.FormattingEnabled = true;
+            this.PlayList1.ItemHeight = 20;
+            this.PlayList1.Location = new System.Drawing.Point(217, 431);
+            this.PlayList1.Name = "PlayList1";
+            this.PlayList1.Size = new System.Drawing.Size(283, 144);
+            this.PlayList1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 805);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 48);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Setup";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(524, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Copyright © 2018 Ed Bee";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.MenuText;
+            this.BackgroundImage = global::WindowsFormsApp3.Properties.Resources.jukebox4;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(737, 858);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PlayList1);
+            this.Controls.Add(this.btn_About);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox_Genre_List);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox_Genre_List;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btn_About;
+        private System.Windows.Forms.ListBox PlayList1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
